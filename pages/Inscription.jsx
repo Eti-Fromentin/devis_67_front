b151f44f6363c3086bc2d716c4ef692c6edc6539
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2022-01-04 12:18:14 +0100

    tests import uikit in components and rendering test in index page

--- pages/Inscription.jsx
+++ pages/Inscription.jsx
@@ -1,46 +1,48 @@
 import React from 'react';
+import '../node_modules/uikit/dist/css/uikit.min.css'
+import '../node_modules/uikit/dist/js/uikit.min.js'
 
 function Inscription() {
   return (
-<div class="uk-card uk-card-default uk-card-hover uk-card-large uk-card-body">
-    <h1 class="uk-card-title uk-text-center">Inscrivez-vous!</h1>
+<div className="data-uk-card data-uk-card-default data-uk-card-hover data-uk-card-large data-uk-card-body">
+    <h1 className="data-uk-card-title data-uk-text-center">Inscrivez-vous!</h1>
       <form>
-        <fieldset class="uk-fieldset">
+        <fieldset className="data-uk-fieldset">
             
-          <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-            <label><input class="uk-radio" type="radio" name="radio2" /> Monsieur</label>
-            <label><input class="uk-radio" type="radio" name="radio2" /> Madame</label>
+          <div className="data-uk-margin data-uk-grid-small data-uk-child-width-auto data-uk-grid">
+            <label><input className="data-uk-radio" type="radio" name="radio2" /> Madame</label>
+            <label><input className="data-uk-radio" type="radio" name="radio2" /> Monsieur</label>
           </div>
-          <div class="uk-margin">
-              <input class="uk-input" type="text" placeholder="Nom*"/>
+          <div className="data-uk-margin">
+              <input className="data-uk-input" type="text" placeholder="Nom*"/>
           </div>
-          <div class="uk-margin">
-              <input class="uk-input" type="text" placeholder="Prénom*"/>
+          <div className="data-data-uk-margin">
+              <input className="data-uk-input" type="text" placeholder="Prénom*"/>
           </div>
-          <div class="uk-margin">
-              <input class="uk-input" type="text" placeholder="Nom d'utilisateur"/>
+          <div className="data-uk-margin">
+              <input className="data-uk-input" type="text" placeholder="Nom d'utilisateur"/>
           </div>
-          <div class="uk-margin">
-              <input class="uk-input" type="text" placeholder="Adresse postale*"/>
+          <div className="data-uk-margin">
+              <input className="data-uk-input" type="text" placeholder="Adresse postale*"/>
           </div>
-          <div class="uk-margin">
-              <input class="uk-input" type="text" placeholder="Code postal*"/>
+          <div className="data-uk-margin">
+              <input className="data-uk-input" type="text" placeholder="Code postal*"/>
           </div>
-          <div class="uk-margin">
-              <input class="uk-input" type="text" placeholder="Adresse e-mail*"/>
+          <div className="data-uk-margin">
+              <input className="data-uk-input" type="text" placeholder="Adresse e-mail*"/>
           </div>
-          <div class="uk-margin">
-              <input class="uk-input" type="text" placeholder="Mot de passe*"/>
+          <div className="data-uk-margin">
+              <input className="data-uk-input" type="text" placeholder="Mot de passe*"/>
           </div>
-          <div class="uk-margin">
-              <input class="uk-input" type="text" placeholder="Confirmer Mot de passe*"/>
+          <div className="data-uk-margin">
+              <input className="data-uk-input" type="text" placeholder="Confirmer Mot de passe*"/>
           </div>
-          <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-            <label><input class="uk-checkbox" type="checkbox"/> J'accepte de recevoir des newletters</label>
+          <div className="data-uk-margin data-uk-grid-small data-uk-child-width-auto data-uk-grid">
+            <label><input className="data-uk-checkbox" type="checkbox"/> J'accepte de recevoir des newletters</label>
         </div>
-          <label><input class="uk-checkbox" type="checkbox"/>En soumettant ce formulaire, j'accepte que les données saisies soient collectées dans le but de traiter ma demande*</label>
-          <p class="uk-margin">
-          <button class="uk-button uk-button-primary uk-button-large">S'inscrire</button>
+          <label><input className="data-uk-checkbox" type="checkbox"/>En soumettant ce formulaire, j'accepte que les données saisies soient collectées dans le but de traiter ma demande*</label>
+          <p className="data-uk-margin">
+          <button className="data-uk-button data-uk-button-primary data-uk-button-large">S'inscrire</button>
           </p>
 
         </fieldset>
