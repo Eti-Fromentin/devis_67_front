bc3b6e31a1d598f5e0276e40efc7174f83467b1e
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2022-01-04 10:22:56 +0100

    form created for page inscription and inscirption link added in page connexion

--- pages/Connexion.jsx
+++ pages/Connexion.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 function Connexion() {
   return (
-<div class="uk-card uk-card-hover uk-card-body">
-    <h1>BIENVENUE!</h1>
+<div class="uk-card uk-card-default uk-card-hover uk-card-large uk-card-body">
+    <h1 class="uk-card-title uk-text-center">BIENVENUE!</h1>
     <p>Connectez-vous grâce à votre adresse e-mail et votre mot de passe.</p>
     <form>
             <fieldset class="uk-fieldset">
@@ -20,11 +20,13 @@ function Connexion() {
             <p class="uk-margin">
             <button class="uk-button uk-button-primary uk-button-large">Connexion</button>
             </p>
+        <p>Pas encore inscrit(e)? cliquez <a href="/inscription">ici</a></p>
 
         </fieldset>
     </form>
     
-    </div>);
-}
+    </div>
+    );
+};
 
-export default Inscription;
+export default Connexion;
--- pages/Inscription.jsx
+++ pages/Inscription.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
 
-function pages2() {
-  return <div>hello again</div>;
-}
+function Inscription() {
+  return (
+<div class="uk-card uk-card-default uk-card-hover uk-card-large uk-card-body">
+    <h1 class="uk-card-title uk-text-center">Inscrivez-vous!</h1>
+      <form>
+        <fieldset class="uk-fieldset">
+            
+          <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+            <label><input class="uk-radio" type="radio" name="radio2" /> Monsieur</label>
+            <label><input class="uk-radio" type="radio" name="radio2" /> Madame</label>
+          </div>
+          <div class="uk-margin">
+              <input class="uk-input" type="text" placeholder="Nom*"/>
+          </div>
+          <div class="uk-margin">
+              <input class="uk-input" type="text" placeholder="Prénom*"/>
+          </div>
+          <div class="uk-margin">
+              <input class="uk-input" type="text" placeholder="Nom d'utilisateur"/>
+          </div>
+          <div class="uk-margin">
+              <input class="uk-input" type="text" placeholder="Adresse postale*"/>
+          </div>
+          <div class="uk-margin">
+              <input class="uk-input" type="text" placeholder="Code postal*"/>
+          </div>
+          <div class="uk-margin">
+              <input class="uk-input" type="text" placeholder="Adresse e-mail*"/>
+          </div>
+          <div class="uk-margin">
+              <input class="uk-input" type="text" placeholder="Mot de passe*"/>
+          </div>
+          <div class="uk-margin">
+              <input class="uk-input" type="text" placeholder="Confirmer Mot de passe*"/>
+          </div>
+          <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+            <label><input class="uk-checkbox" type="checkbox"/> J'accepte de recevoir des newletters</label>
+        </div>
+          <label><input class="uk-checkbox" type="checkbox"/>En soumettant ce formulaire, j'accepte que les données saisies soient collectées dans le but de traiter ma demande*</label>
+          <p class="uk-margin">
+          <button class="uk-button uk-button-primary uk-button-large">S'inscrire</button>
+          </p>
 
-export default pages2;
+        </fieldset>
+      </form>
+    
+    </div>
+  );
+
+};
+
+export default Inscription;
