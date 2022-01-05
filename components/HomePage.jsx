175dcec5b8d28bc8a50ca7dc6e80be8710a6f6c2
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2022-01-05 15:28:30 +0100

    margin added to HomePage cards

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage () {
      <div className="uk-container uk-position-center">
     <div className="uk-column-1-2 uk-column-divider">
     <div>
-        <div className="uk-card uk-card-default uk-card-large uk-margin-medium">
+        <div className="uk-card uk-card-default uk-card-large uk-margin">
             <div className="uk-card-body">
                 <h3 className="uk-card-title">Devis</h3>
                 <p>Trouvez l'artisan qu'il vous faut et demandez un devis.</p>
@@ -22,7 +22,7 @@ function HomePage () {
         </div>
     </div>
     <div>
-        <div className="uk-card uk-card-primary uk-card-large">
+        <div className="uk-card uk-card-primary uk-card-large uk-margin">
             <div className="uk-card-body">
                 <h3 className="uk-card-title">Aides</h3>
                 <p>Estimez le montant de vos aides à la rénovation.</p>
