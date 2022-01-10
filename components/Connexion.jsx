b08a33656eaa858f66342c6a68575372a3d448ee
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2022-01-10 11:19:17 +0100

    pages made prettier with ES lint

--- components/Connexion.jsx
+++ components/Connexion.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import Link from 'next/link';
 import { Card, Container, Form, Button, Row, Col } from 'react-bootstrap';
-import styles from '../styles/Connexion.module.css'
-
+import styles from '../styles/Connexion.module.css';
 
 function Connexion() {
   return (
-<div className={styles.connexion}>
-  <Container>
-    <Row className="justify-content-md-center">
-      <Col md={8}>
-    <Card className={styles.card}>
-    <h1 className="">BIENVENUE!</h1>
-    <p>Connectez-vous grâce à votre adresse e-mail et votre mot de passe.</p>
-        <Card.Body>
-            <Form>
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label>Adresse Email*</Form.Label>
-              <Form.Control size="sm" type="text" placeholder="Entrez votre adresse email" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="password">
-              <Form.Label>Mot de passe*</Form.Label>
-              <Form.Control size="sm" type="password" placeholder="Insérez votre mot de passe" />
-            </Form.Group>
-            <p>
-            <Link href="">Mot de passe oublié?</Link>
-            </p>
-            <Button className={styles.button} variant="primary" type="submit">
-              Connexion
-            </Button>
-            <p>Pas encore inscrit(e)? cliquez <Link href="/inscription">ici</Link></p>
-            </Form>
-        </Card.Body>
-    </Card>
-    </Col>
-    </Row>
-    </Container>
+    <div className={styles.connexion}>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md={8}>
+            <Card className={styles.card}>
+              <h1 className="">BIENVENUE!</h1>
+              <p>Connectez-vous grâce à votre adresse e-mail et votre mot de passe.</p>
+              <Card.Body>
+                <Form>
+                  <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Adresse Email*</Form.Label>
+                    <Form.Control size="sm" type="text" placeholder="Entrez votre adresse email" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>Mot de passe*</Form.Label>
+                    <Form.Control size="sm" type="password" placeholder="Insérez votre mot de passe" />
+                  </Form.Group>
+                  <p>
+                    <Link href="">Mot de passe oublié?</Link>
+                  </p>
+                  <Button className={styles.button} variant="primary" type="submit">
+                    Connexion
+                  </Button>
+                  <p>
+                    Pas encore inscrit(e)? cliquez <Link href="/inscription">ici</Link>
+                  </p>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
-    );
-};
+  );
+}
 
 export default Connexion;
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -11,33 +11,33 @@ function HomePage() {
     <div className={styles.home}>
       <Container>
         <Row>
-          <Col md={{order: 'first'}}>
-      <Card className={styles.homecard}>
-        <Card.Title>Formulaire Devis</Card.Title>
-        <Card.Body>
-          <Card.Text>
-            Trouvez l'artisan qu'il vous faut et demandez un devis <Link href="/devis">ici</Link>
-          </Card.Text>
-          <div>
-          <Image className={styles.homeImage} src={image1} alt="Image artisan" priority="true" placeholder="blur" />
-          </div>
-        </Card.Body>
-      </Card>
-      </Col>
-      <Col md={{order: 'second'}}>
-      <Card className={styles.homecard}>
-        <Card.Title>Calcul des Aides</Card.Title>
-        <Card.Body>
-          <Card.Text>
-            Estimez le montant de vos aides à la rénovation <Link href="/aides">ici</Link>
-          </Card.Text>
-          <div>
-          <Image src={image2} alt="Image billets" priority="true" placeholder="blur" />
-          </div>
-        </Card.Body>
-      </Card>
-      </Col>
-      </Row>
+          <Col md={{ order: 'first' }}>
+            <Card className={styles.homecard}>
+              <Card.Title>Formulaire Devis</Card.Title>
+              <Card.Body>
+                <Card.Text>
+                  Trouvez l'artisan qu'il vous faut et demandez un devis <Link href="/devis">ici</Link>
+                </Card.Text>
+                <div>
+                  <Image className={styles.homeImage} src={image1} alt="Image artisan" priority="true" placeholder="blur" />
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={{ order: 'second' }}>
+            <Card className={styles.homecard}>
+              <Card.Title>Calcul des Aides</Card.Title>
+              <Card.Body>
+                <Card.Text>
+                  Estimez le montant de vos aides à la rénovation <Link href="/aides">ici</Link>
+                </Card.Text>
+                <div>
+                  <Image src={image2} alt="Image billets" priority="true" placeholder="blur" />
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
