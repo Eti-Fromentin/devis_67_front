cf13c5b6802c56d8035fbafc3368e7075b8c8965
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2022-01-11 18:46:09 +0100

    inscription form created with react-hook-form and bootstrap in InscriptionBis component, css module created for this component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,14 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Inscription from '../components/Inscription';
 import Connexion from '../components/Connexion';
 import HomePage from './HomePage';
+import InscriptionBis from '../components/InscriptionBis';
 
 
 function MyApp() {
   return (
     <>
-    <HomePage />
-    <Inscription />
     <Connexion />
+    <InscriptionBis />
     </>
   );
 }
