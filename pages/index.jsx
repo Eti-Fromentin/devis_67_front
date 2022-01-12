59eb840a6430231ce2e42462842a7260ed767fa4
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2022-01-12 15:33:52 +0100

    new branch created with old HomePage in index.jsx and old css module but responsive in case we need it

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,37 +12,37 @@ function HomePage() {
     <div>
       <NavBar pageType="devis" />
       <div className={styles.home}>
-        <Container className={styles.containerHomepage}>
-          <Row>
-            <Col md={{ order: 'first' }}>
-              <Card className={styles.homecard1}>
-                <Card.Title className={styles.cardTitleHome}>Formulaire Devis</Card.Title>
-                <Card.Body>
-                  <Card.Text className={styles.cardtexthome}>
-                    Trouvez l&#039; artisan qu&#039; il vous faut et demandez un devis <Link href="/devis">ici</Link>
-                  </Card.Text>
-                  <div className={styles.homeImage1}>
-                    <Image width={250} height={220} src={image1} alt="Image artisan" priority="true" placeholder="blur" />
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={{ order: 'second' }}>
-              <Card className={styles.homecard2}>
-                <Card.Title className={styles.cardTitleHome}>Calcul des Aides</Card.Title>
-                <Card.Body>
-                  <Card.Text className={styles.cardtexthome}>
-                    Estimez le montant de vos aides à la rénovation <Link href="/aide">ici</Link>
-                  </Card.Text>
-                  <div className={styles.homeImage2}>
-                    <Image src={image2} width={250} height={220} alt="Image billets" priority="true" placeholder="blur" />
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container>
+        <Row>
+          <Col md={{ order: 'first' }}>
+            <Card className={styles.homecard}>
+              <Card.Title>Formulaire Devis</Card.Title>
+              <Card.Body>
+                <Card.Text>
+                  Trouvez l'artisan qu'il vous faut et demandez un devis <Link href="/devis">ici</Link>
+                </Card.Text>
+                <div>
+                  <Image width="400" height="300" src={image1} alt="Image artisan" priority="true" placeholder="blur" />
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={{ order: 'second' }}>
+            <Card className={styles.homecard}>
+              <Card.Title>Calcul des Aides</Card.Title>
+              <Card.Body>
+                <Card.Text>
+                  Estimez le montant de vos aides à la rénovation <Link href="/aide">ici</Link>
+                </Card.Text>
+                <div>
+                  <Image width="400" height="300" src={image2} alt="Image billets" priority="true" placeholder="blur" />
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
     </div>
   );
 }
