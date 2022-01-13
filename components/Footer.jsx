48d5790e875f73b1f44f4092005c79dc99e55bc2
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2022-01-13 11:55:06 +0100

    Footer Navbar Added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from '../public/logo.png';
-import Image from 'next/image';
+//import logo from '../public/logo.png';
+//import Image from 'next/image';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+//import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function Footer() {
   const [footerData, setFooterData] = useState([]);
@@ -19,34 +19,33 @@ function Footer() {
       {!footerData.length ? (
         <p>Loading</p>
       ) : (
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand href="/">
-              <Image src={logo} alt="image logo" />
-            </Navbar.Brand>
-          </Container>
-          <Container>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                {footerData.length &&
-                  footerData.map((element) => {
-                    if (element.category === 'titre') {
-                      return <h3 key={element.id}>{element.text}</h3>;
-                    } else {
-                      return (
-                        <div key={element.id}>
-                          <Nav.Link href={element.pages.url}>{element.text}</Nav.Link>
-                        </div>
-                      );
-                    }
-                  })}
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <div>
+          <ul>
+            {footerData
+              .filter((element) => element.section === 'list1')
+              .map((element) => {
+                if (element.category === 'titre') {
+                  return <h4>{element.text}</h4>;
+                } else {
+                  return <li> {element.text}</li>;
+                }
+              })}
+          </ul>
+          <ul>
+            {footerData
+              .filter((element) => element.section === 'list2')
+              .map((element) => {
+                if (element.category === 'titre') {
+                  return <h4>{element.text}</h4>;
+                } else {
+                  return <li> {element.text}</li>;
+                }
+              })}
+          </ul>
+        </div>
       )}
     </div>
   );
 }
+
 export default Footer;
