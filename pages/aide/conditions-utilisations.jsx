e9948e3186c388d896964ac2b31f40d4845ca784
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2022-01-18 11:31:45 +0100

    Conditions Page Added Css Needed

--- pages/aide/conditions-utilisations.jsx
+++ pages/aide/conditions-utilisations.jsx
@@ -1,13 +1,67 @@
 import React from 'react';
+import axios from 'axios';
 import NavBar from '../../components/NavBar';
+import styles from '../../styles/Conditions.module.css';
+import { Container } from 'react-bootstrap';
 
-function ConditionsUtilisations() {
+function ConditionsUtilisations({ posts }) {
   return (
     <div>
       <NavBar pageType="devis" />
-      Page Conditions d utilisations
+      <h1>Conditions d utilisations</h1>
+      {!posts ? (
+        <p>Loading</p>
+      ) : (
+        <div>
+          <ul>
+            {posts
+              .filter((itemsMenu) => itemsMenu.page_section === 'chapterTitle')
+              .map((itemMenu) => (
+                <li className={styles.itemsMenu} key={itemMenu.key}>
+                  {itemMenu.text}
+                </li>
+              ))}
+          </ul>
+          <Container>
+            {posts.map((itemMenu) => {
+              //pour chaque position un div key={itemMenu.position}
+              //for (let i = 0; i < itemMenu.lenght; i++) {
+              //if (itemMenu.position === i) {
+              if (itemMenu.page_section === 'chapterTitle') {
+                return (
+                  <div key={itemMenu.position}>
+                    <h5 className={styles.titleConditions} key={itemMenu.position}>
+                      {itemMenu.text}
+                    </h5>
+                  </div>
+                );
+              } else if (itemMenu.page_section === 'text') {
+                return (
+                  <div key={itemMenu.position}>
+                    <p className={styles.textConditions}>{itemMenu.text}</p>
+                  </div>
+                );
+              }
+              //}
+              //}
+            })}
+          </Container>
+        </div>
+      )}
     </div>
   );
+}
+export async function getStaticProps() {
+  const posts = await axios
+    .get('http://localhost:8000/api/pagescontent')
+    .then((response) => response.data)
+    .then((data) => data.filter((element) => element.visible === 1));
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 
 export default ConditionsUtilisations;
