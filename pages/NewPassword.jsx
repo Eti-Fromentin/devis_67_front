5c72c9aa975ee27ed61b38c4ad8e740c5f53df06
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2022-01-18 10:18:18 +0100

    NewPassword page created with its Css module and link added in Connexion and NewPassword

---
+++ pages/NewPassword.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap';
+import NavBar from '../components/NavBar';
+import styles from '../styles/NewPassword.module.css';
+
+function NewPassword () {
+
+const ConnectSchema = Yup.object().shape({
+  email: Yup.string().required('⚠ Un Email est requis').email('⚠ Email invalide'),
+  password: Yup.string().required('⚠ Mot de passe requis').min(8, '⚠ Minimum de 8 caractères').max(50, '⚠ Maximum de 50 caractères'),
+});
+
+const {
+  register,
+  handleSubmit,
+  formState: { errors },
+} = useForm({
+  resolver: yupResolver(ConnectSchema),
+});
+
+const ConnectSubmit = (data) => {
+  console.log(JSON.stringify(data, null, 2));
+};
+
+return (
+  <div>
+    <NavBar pageType="devis" />
+    <div className={styles.connexion}>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md={8}>
+            <Card className={styles.card}>
+              <h1 className="">Récupérer mon mot de passe</h1>
+              <p>Renseignez les informations ci-dessous.</p>
+              <p>Nous vous enverrons un lien pour récupérer votre accès à votre compte Devis67</p>
+              <Card.Body>
+                <Form onSubmit={handleSubmit(ConnectSubmit)}>
+                  <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Adresse Email*</Form.Label>
+                    <Form.Control {...register('email')} size="sm" type="text" placeholder="Entrez votre adresse email" />
+                    <p className={styles.error}>{errors.email && errors.email?.message}</p>
+                  </Form.Group>
+                  <Button className={styles.button} variant="primary" type="submit">
+                    Envoyer le lien
+                  </Button>
+                  <p>
+                    <Link href="/espace-client">Revenir à la connexion</Link>
+                  </p>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  </div>
+);
+}
+
+export default NewPassword;
