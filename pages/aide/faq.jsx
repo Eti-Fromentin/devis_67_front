d5338e08a7d7f68ab5a4c619614c517bd602ffc6
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2022-01-25 11:26:10 +0100

    Faq page created

--- pages/aide/faq.jsx
+++ pages/aide/faq.jsx
@@ -1,42 +1,60 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
-//import axios from 'axios';
+import axios from 'axios';
 import styles from '../../styles/Faq.module.css';
 import { Container } from 'react-bootstrap';
 
-function Faq() {
+function Faq({ posts }) {
   return (
     <div>
       <NavBar pageType="devis" />
-
-      <h1>FAQ</h1>
+      <h1 className={styles.h1Faq}>Foire aux questions</h1>
       <Container>
-        <div className={styles.containerFaq}>
-          <h1>FAQ</h1>
-          <h1>FAQ</h1>
-          <h1>FAQ</h1>
-          <h1>FAQ</h1>
-          <h1>FAQ</h1>
-          <h1>FAQ</h1>
-        </div>
+        {!posts ? (
+          <p>Loading</p>
+        ) : (
+          <div className={styles.containerFaq}>
+            <div className={styles.textFaq}>
+              <div>
+                {posts.map((itemMenu) => {
+                  if (itemMenu.page_section === 'title') {
+                    return (
+                      <div key={itemMenu.position}>
+                        <h5 className={styles.titleFaq} id={itemMenu.key} key={itemMenu.position}>
+                          {itemMenu.text}
+                        </h5>
+                      </div>
+                    );
+                  } else if (itemMenu.page_section === 'text') {
+                    return (
+                      <div key={itemMenu.position}>
+                        <p className={styles.textConditions}>{itemMenu.text}</p>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+            </div>
+          </div>
+        )}
       </Container>
 
       <Footer pageType="devis" />
     </div>
   );
 }
-// export async function getStaticProps() {
-//   const posts = await axios
-//     .get('http://localhost:8000/api/pagescontent')
-//     .then((response) => response.data)
-//     .then((data) => data.filter((element) => element.visible === 1 && element.page_name === 'modeEmploi'));
+export async function getStaticProps() {
+  const posts = await axios
+    .get('http://localhost:8000/api/pagescontent')
+    .then((response) => response.data)
+    .then((data) => data.filter((element) => element.visible === 1 && element.page_name === 'faq'));
 
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// }
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Faq;
