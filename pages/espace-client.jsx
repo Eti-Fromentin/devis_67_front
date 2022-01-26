c76453cb3f0296048095c7f2dc847afdc5073436
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2022-01-26 14:19:18 +0100

    Context Added

--- pages/espace-client.jsx
+++ pages/espace-client.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-// import Connexion from '../components/Connexion';
+import Connexion from '../components/Connexion';
 import UserProfile from './[user]';
+import loginContext from '../contexts/loginContext';
 
 function EspaceClient() {
+  const { isLogin } = useContext(loginContext);
+  const loggedin = localStorage.getItem('AccessToken');
+  {
+    loggedin ? isLogin === true : isLogin === false;
+  }
   return (
     <div>
       <NavBar pageType="devis" />
+      {isLogin && isLogin === true ? <UserProfile /> : <Connexion />}
       {/* <Connexion /> */}
-      <UserProfile />
       <Footer pageType="devis" />
     </div>
   );
