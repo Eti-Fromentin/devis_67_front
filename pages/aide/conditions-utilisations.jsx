37ef0c7098644d92e5823db917685d5940bd0660
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2022-01-27 11:21:13 +0100

    Link internal

--- pages/aide/conditions-utilisations.jsx
+++ pages/aide/conditions-utilisations.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import Head from 'next/head';
 import axios from 'axios';
-
-//import { HashLink as Link } from 'react-router-hash-link';
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
 import styles from '../../styles/Conditions.module.css';
 
-function ConditionsUtilisations({ posts, dataHead }) {
-  console.log(posts);
+function ConditionsUtilisations({ posts }) {
   return (
     <div>
-      <div>
-        <Head>
-          <title>{dataHead.title}</title>
-          <meta name="description" content={dataHead.description} />
-          <meta name="keywords" content={dataHead.keywords} />
-        </Head>
-      </div>
       <div>
         <NavBar pageType="devis" />
         <h1>Conditions d&#039; utilisations</h1>
@@ -31,21 +20,8 @@ function ConditionsUtilisations({ posts, dataHead }) {
                   .filter((itemsMenu) => itemsMenu.page_section === 'chapterTitle')
                   .map((itemMenu) => (
                     <li className={styles.itemsMenu} key={itemMenu.key}>
-                      {itemMenu.text}
+                      <a href={`#${itemMenu.text}`}>{itemMenu.text}</a>
                     </li>
-                    //<Link className={styles.itemsMenu} key={itemMenu.key} href={href}>
-                    //  <a>{itemMenu.text}</a>
-                    //</Link>
-
-                    // function NavLink({ href, name }) {
-                    //   return (
-                    //     <Link href={href}>
-                    //       <a>{name}</a>
-                    //     </Link>
-                    //   )
-                    // }
-
-                    //export default NavLink
                   ))}
               </ul>
               <div>
@@ -53,7 +29,7 @@ function ConditionsUtilisations({ posts, dataHead }) {
                   if (itemMenu.page_section === 'chapterTitle') {
                     return (
                       <div key={itemMenu.position}>
-                        <h5 className={styles.titleConditions} id={itemMenu.key} key={itemMenu.position}>
+                        <h5 className={styles.titleConditions} id={itemMenu.text} key={itemMenu.position}>
                           {itemMenu.text}
                         </h5>
                       </div>
@@ -80,15 +56,9 @@ export async function getStaticProps() {
     .get('http://localhost:8000/api/pagescontent/conditions')
     .then((response) => response.data)
     .then((data) => data.filter((element) => element.visible === 1));
-  const dataHead = await axios
-    .get('http://localhost:8000/api/pages')
-    .then((response) => response.data)
-    .then((data) => data.filter((elements) => elements.url === '/aide/conditions-utilisations'));
-
   return {
     props: {
       posts,
-      dataHead,
     },
   };
 }
