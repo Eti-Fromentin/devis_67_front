c917fd4c63b9426bbe0147716196400cb7ba5233
Eti-Fromentin <fromentin.etienne@gmail.com>
2022-01-28 11:24:45 +0100

    radio repaired

--- pages/devis/[params].jsx
+++ pages/devis/[params].jsx
@@ -50,9 +50,10 @@ function DevisAllQuestions({ form, headInfo }) {
                 <FormCheck
                   inline
                   key={elt.id}
+                  type="radio"
                   label={elt.formulaire_possible_answer.answer}
-                  name={elt.formulaire_id}
                   value={elt.formulaire_possible_answer.answer}
+                  name={elt.formulaire_id}
                 />
               </Card>
             ))}
