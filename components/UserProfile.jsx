842e3a257770665fb2222c888ffa82b196e32e79
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2022-01-31 16:48:03 +0100

    Css module renamed from EspaceClient to UserProfile to match the UserProfile component called in espace-client page

--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import LoginContext from '../contexts/loginContext';
 import { Accordion, Tabs, Tab, Spinner } from 'react-bootstrap';
-import styles from '../styles/EspaceClient.module.css';
+import styles from '../styles/UserProfile.module.css';
 
 function UserProfile() {
   const { userData, getUserData, isLogin } = useContext(LoginContext);
@@ -20,66 +20,68 @@ function UserProfile() {
         </div>
       ) : (
         <div>
-        {userData && (
-        <div className={styles.profileContainer}>
-          <Tabs defaultActiveKey="UserProfile" id="uncontrolled-tab-example" className="justify-content-center">
-            <Tab eventKey="UserProfile" title="Mes Informations">
-              <div className={styles.profileData}>
-                <ul>
-                  <li> Prénom: {userData && userData.firstname} </li>
-                  <li> Nom: {userData && userData.lastname}</li>
-                  <li> Email: {userData && userData.email}</li>
-                  <li> Téléphone: {userData && userData.phone ? userData.phone : 'non renseigné'}</li>
-                  <li> Adresse: {userData && userData.address ? userData.address : 'non renseigné'}</li>
-                  <li> Code Postal: {userData && userData.postalcode}</li>
-                  <li> Ville: {userData && userData.city}</li>
-                </ul>
-              </div>
-            </Tab>
-            <Tab eventKey="UserDevis" title="Mes Devis">
-              <div className={styles.profileMain}>
-                {!userData && !userData.devis.length ? (
-                  <p>Aucun devis effectué</p>
-                ) : (
-                  userData.devis.map((elt) => (
-                    <li>
-                      <div className={styles.headerDevis}>
-                        <p>Devis du {elt.created_at.slice(0, 10)}</p>
-                        <p>Catégorie: {elt.categories_devis_provider.title}</p>
-                        <p>Statut : {elt.status === 0 ? 'Envoyé' : 'en cours de traitement'}</p>
-                      </div>
-                      <Accordion>
-                        <Accordion.Header>Détails</Accordion.Header>
-                        <Accordion.Body>
-                          {elt.questions_answers.map((res) => (
-                            <div className={styles.profileDevis}>
-                              <p>Question: {res.questions}</p>
-                              <p>Réponse: {res.answers}</p>
-                            </div>
-                          ))}
-                        </Accordion.Body>
-                      </Accordion>
-                    </li>
-                  ))
-                )}
-              </div>
-            </Tab>
-            <Tab eventKey="UserMessages" title="Mes Messages">
-              <div className={styles.profileMessages}>
-              {!userData && !userData.messages.length ? (
-                  <p>Aucun message envoyé</p>
-                ) : (
-                  userData.messages.map((elt) => (
-                      <div className={styles.headerMessages}>
-                        <p>Message du {elt.created_at.slice(0, 10)}</p>
-                        <p>Sujet: {elt.subject}</p>
-                        <p>Texte : {elt.text}</p>
-                      </div>)))}
-              </div>
-            </Tab>
-          </Tabs>
-        </div>
-        )}
+          {userData && (
+            <div className={styles.profileContainer}>
+              <Tabs defaultActiveKey="UserProfile" id="uncontrolled-tab-example" className="justify-content-center">
+                <Tab eventKey="UserProfile" title="Mes Informations">
+                  <div className={styles.profileData}>
+                    <ul>
+                      <li> Prénom: {userData && userData.firstname} </li>
+                      <li> Nom: {userData && userData.lastname}</li>
+                      <li> Email: {userData && userData.email}</li>
+                      <li> Téléphone: {userData && userData.phone ? userData.phone : 'non renseigné'}</li>
+                      <li> Adresse: {userData && userData.address ? userData.address : 'non renseigné'}</li>
+                      <li> Code Postal: {userData && userData.postalcode}</li>
+                      <li> Ville: {userData && userData.city}</li>
+                    </ul>
+                  </div>
+                </Tab>
+                <Tab eventKey="UserDevis" title="Mes Devis">
+                  <div className={styles.profileMain}>
+                    {!userData && !userData.devis.length ? (
+                      <p>Aucun devis effectué</p>
+                    ) : (
+                      userData.devis.map((elt) => (
+                        <li>
+                          <div className={styles.headerDevis}>
+                            <p>Devis du {elt.created_at.slice(0, 10)}</p>
+                            <p>Catégorie: {elt.categories_devis_provider.title}</p>
+                            <p>Statut : {elt.status === 0 ? 'Envoyé' : 'en cours de traitement'}</p>
+                          </div>
+                          <Accordion>
+                            <Accordion.Header>Détails</Accordion.Header>
+                            <Accordion.Body>
+                              {elt.questions_answers.map((res) => (
+                                <div className={styles.profileDevis}>
+                                  <p>Question: {res.questions}</p>
+                                  <p>Réponse: {res.answers}</p>
+                                </div>
+                              ))}
+                            </Accordion.Body>
+                          </Accordion>
+                        </li>
+                      ))
+                    )}
+                  </div>
+                </Tab>
+                <Tab eventKey="UserMessages" title="Mes Messages">
+                  <div className={styles.profileMessages}>
+                    {!userData && !userData.messages.length ? (
+                      <p>Aucun message envoyé</p>
+                    ) : (
+                      userData.messages.map((elt) => (
+                        <div className={styles.headerMessages}>
+                          <p>Message du {elt.created_at.slice(0, 10)}</p>
+                          <p>Sujet: {elt.subject}</p>
+                          <p>Description: {elt.text}</p>
+                        </div>
+                      ))
+                    )}
+                  </div>
+                </Tab>
+              </Tabs>
+            </div>
+          )}
         </div>
       )}
     </div>
